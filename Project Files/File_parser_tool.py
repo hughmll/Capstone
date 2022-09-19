7fbf25cf54c347ccf2cd2d12c53b5be18b36e84d
Hugh <hughmichaellyons@gmail.com>
2022-09-19 23:22:36 +1000

    moved decoding and splitting on /n into function

--- Project Files/File_parser_tool.py
+++ Project Files/File_parser_tool.py
@@ -2,21 +2,22 @@ with open("test_conf_file.txt", "rb") as bytes_text:
     data = bytes_text.read()
 
 var_dict = {}
-data = data.decode().split("\n")
+def file_parser(x):
+    data = x.decode().split("\n")
+    for item in data:
+        if "[Interface]" in item:
+            item.replace("[Interface]", "")
+        elif "\r" in item:
+            item = item.replace("\r", "")
+            key = item.split(" = ")[0]
+            value = item.split(" = ")[1]
+            var_dict[key] = value
+        else:
+            key = item.split(" = ")[0]
+            value = item.split(" = ")[1]
+            var_dict[key] = value
 
-for item in data:
-    if "[Interface]" in item:
-        item.replace("[Interface]", "")
-    elif "\r" in item:
-        item = item.replace("\r", "")
-        key = item.split(" = ")[0]
-        value = item.split(" = ")[1]
-        var_dict[key] = value
-    else:
-        key = item.split(" = ")[0]
-        value = item.split(" = ")[1]
-        var_dict[key] = value
-
+file_parser(data)
 print(var_dict)
 
 
