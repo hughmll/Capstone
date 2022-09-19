60bb5a7baa74285b6f84de308fb7c35e7d35c088
Hugh <hughmichaellyons@gmail.com>
2022-09-19 22:48:11 +1000

    Update to Encrypt Decrypt function to read nonce from file based on byte offset (first 16 bytes of text file).

--- Project Files/Encrypt_Decrypt.py
+++ Project Files/Encrypt_Decrypt.py
@@ -1,32 +1,32 @@
 from Crypto.Cipher import AES
 from PBKDF import *
 
-data = "This is plaintext. You can read it"
-print(data)
-
 encrypted_data=[]
 
 def encryption():
+    with open("test_conf_file.txt", "rb") as plaintext_file:
+        plaintext_data = plaintext_file.read()
+    print(plaintext_data)
     key = key_derivation()
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
-    print(f"nonce = {nonce}")
-    cipher_text, tag = cipher.encrypt_and_digest(data.encode('UTF-8'))
+    #print(nonce)
+    cipher_text, tag = cipher.encrypt_and_digest(plaintext_data)
     print(cipher_text)
-    encrypted_data.append(nonce)
-    encrypted_data.append(cipher_text)
-    #with open('Encrypted_text_file.txt', 'a') as encrypted_file:
-        #encrypted_file.write(nonce.hex(sep = ' '))
-        #encrypted_file.write("\n")
+    with open("Encrypted_conf_file.txt", "wb") as encrypted_file:
+        encrypted_file.write(nonce)
+        encrypted_file.write(cipher_text)
         #encrypted_file.write(cipher_text.hex(sep = ' '))
-
 
 encryption()
 
 def decryption():
     key = key_derivation()
-    cipher = AES.new(key, AES.MODE_EAX, nonce=encrypted_data[0])
-    deciphered_text = cipher.decrypt(encrypted_data[1])
-    print(deciphered_text.decode('UTF-8'))
+    with open("Encrypted_conf_file.txt",  "rb") as encrypted_data:
+        nonce = encrypted_data.read(16)
+        cipher_text2 = encrypted_data.read()
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    deciphered_text = cipher.decrypt(cipher_text2)
+    print(deciphered_text)
 
 decryption()
