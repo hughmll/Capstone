04c561641feb71e0e19e3e2f5794eea26ca9aa04
Hugh <hughmichaellyons@gmail.com>
2022-11-09 21:51:34 +1000

    - Initial GUI structure

--- Project Files/GUI.py
+++ Project Files/GUI.py
@@ -1,4 +1,7 @@
 import PySimpleGUI as sg
+from File_parser_tool import file_parser
+from PBKDF import key_derivation
+
 
 layout = [[sg.Text('Click browse to find your configuration file'), sg.InputText(readonly=True), sg.FileBrowse('Browse', key='-FILE-')],
           [sg.Text('Enter password to decrypt file here         '), sg.InputText()],
@@ -11,20 +14,24 @@ layout = [[sg.Text('Click browse to find your configuration file'), sg.InputText
 window = sg.Window('Wireguard Client', layout)
 var_dict = {}
 
-def file_parser(x):
-    data = x.decode().split("\n")
-    for item in data:
-        if "[Interface]" in item:
-            item.replace("[Interface]", "")
-        elif "\r" in item:
-            item = item.replace("\r", "")
-            key = item.split(" = ")[0]
-            value = item.split(" = ")[1]
-            var_dict[key] = value
-        else:
-            key = item.split(" = ")[0]
-            value = item.split(" = ")[1]
-            var_dict[key] = value
+def decryption(password):
+    password = password.encode()
+    key = key_derivation(password)
+    with open("Encrypted_conf_file.txt",  "rb") as encrypted_data:
+        nonce = encrypted_data.read(16) #reads first 16 bytes of encrypted file to grab nonce value for passing to key object generator
+    with open("Encrypted_conf_file.txt", "rb") as encrypted_data:
+        encrypted_data.seek(16) #opens file again and moves  to 16 byte offset from beginning of file
+        tag = encrypted_data.read(16) #reads only 16 bytes after 16 byte offset to get MAC tag for verification
+    with open("Encrypted_conf_file.txt", "rb") as encrypted_data:
+        encrypted_data.seek(32) #moves to 32 byte offset from beginning of file (to skip nonce and tag)
+        cipher_text2 = encrypted_data.read() # reads rest of file after offset to get ciphertext only
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce) # new encryption object instantiated for decryption
+    #deciphered_text = cipher.decrypt_and_verify(cipher_text2) #ciphertext decrypted and tag verified
+    try:
+        deciphered_text = cipher.decrypt_and_verify(cipher_text2, tag) #ciphertext decrypted and tag verified
+    except ValueError:
+        print("Decryption failed. Either your password is incorrect or the file has been altered")
+        decryption()
 
 while True:
     event,values = window.read()
@@ -33,7 +40,7 @@ while True:
     elif event == 'Decrypt':
         with open(values['-FILE-'], 'rb') as test_file:
             data = test_file.read()
-            file_parser(data)
+            file_parser(data, var_dict)
             window['-ADDRESS-'].update(var_dict['Address'])
             window['-DNS-'].update(var_dict['DNS'])
 
