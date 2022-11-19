29b91f0aa9d8d8dd3a0bdcdd981870c4459e9c15
Hugh <hughmichaellyons@gmail.com>
2022-11-19 16:55:41 +1000

    - added func for writing conf file

--- Project Files/GUI.py
+++ Project Files/GUI.py
@@ -22,29 +22,34 @@ layout = [[sg.Text('Click browse to find your configuration file'), sg.InputText
 window = sg.Window('Wireguard Client', layout)
 var_dict = {}
 decrypted_text = ""
-tempdir = tempfile.gettempdir()
-filename = '\\wgtunnel.conf'
-name_only = 'wgtunnel'
+tempdir = tempfile.gettempdir() #gets default temp directory relative to user
+filename = '\\wgtunnel.conf' #sets name for temp .conf file that will be used for tunnel establishment
+name_only = 'wgtunnel' #used to identify tunnel interface when passing tunnel tear down command
 
+#the write_comf_file function is needed for the following reasons:
+#> WG is invoked via cmd and requires .conf file as input to begin tunnel session
+#> WG Windows specifically requires CR LF characters after each line with the exception of the last line to parse the .conf file correctly
+# the function below writes the conf file to be used to establish the tunnel session with CR LF after each line (including the blank link separating
+# client from peer (server). It pulls details from the var_dict dictionary storing the data parsed from the decrypted conf file
 def write_conf_file():
-    with open(f"{tempdir}{filename}", "a") as conf_file:
-        conf_file.write("[INTERFACE]\r\n")
-        conf_file.write("PrivateKey = " + var_dict[PrivateKey] + "\r\n")
-        conf_file.write("Address = " + var_dict[Address] + "\r\n")
-        conf_file.write("DNS = " + var_dict[DNS] + "\r\n")
-        conf_file.write("\r\n")
-        conf_file.write("[PEER]\r\n")
-        conf_file.write("PublicKey = " + var_dict[PublicKey] + "\r\n")
-        conf_file.write("AllowedIPs = " + var_dict[AllowedIPs] + "\r\n")
-        conf_file.write("Endpoint = " + var_dict[Endpoint])
+    with open(f"{tempdir}{filename}", "w", newline='\r\n') as conf_file:
+        conf_file.write("[Interface]\n")
+        conf_file.write("PrivateKey = " + var_dict['PrivateKey'] + "\n")
+        conf_file.write("Address = " + var_dict['Address'] + "\n")
+        conf_file.write("DNS = " + var_dict['DNS'] + "\n")
+        conf_file.write("\n")
+        conf_file.write("[Peer]\n")
+        conf_file.write("PublicKey = " + var_dict['PublicKey'] + "\n")
+        conf_file.write("AllowedIPs = " + var_dict['AllowedIPs'] + "\n")
+        conf_file.write("Endpoint = " + var_dict['Endpoint'])
 
 
 
 def decryption(password, file):
     global decrypted_text
-    password = password.encode()
-    file = file
-    key = key_derivation(password)
+    password = password.encode() #converts passed utf-8 byte string (decrypt function only works with byte strings)
+    file = file #file for decryption
+    key = key_derivation(password) #generates symmetric key instance to perform decryption
     with open(file,  "rb") as encrypted_data:
         nonce = encrypted_data.read(16) #reads first 16 bytes of encrypted file to grab nonce value for passing to key object generator
     with open(file, "rb") as encrypted_data:
@@ -64,13 +69,10 @@ def decryption(password, file):
 
 while True:
     event,values = window.read()
-    tunnel = False
-    if tunnel == True:
-        window['-CONNECT-'].update(visible=False)
-        window['-DISCONNECT-'].update(visible=True)
+    tunnel = False #updated to True when connect is pressed and tunnel established. Updated to False again when disconnect pressed
     if event == sg.WIN_CLOSED or event == 'Exit':
         exit()
-    if event == 'Decrypt':
+    if event == 'Decrypt': #when decrypt button is pressed
         window['-ERROR-'].update('')
         #with open(values['-FILE-'], 'rb') as test_file:
         result = decryption(values['-PASSWORD-'], values['-FILE-'])
@@ -85,16 +87,15 @@ while True:
         window['-DNS-'].update(var_dict['DNS'])
         window['-CONNECT-'].update(visible=True)
         print(var_dict)
-        #with open(f"{tempdir}{filename}", "w") as conf_file:
-
-    if event == '-CONNECT-':
-        print('hello there')
+        print(var_dict["PrivateKey"])
+        write_conf_file()
+    if event == '-CONNECT-': #when connect button is pressed
         tunnel = True
-        window['-CONNECT-'].update(visible=False)
-        window['-DISCONNECT-'].update(visible=True)
-    if event == '-DISCONNECT-':
+        window['-CONNECT-'].update(visible=False) #makes connect button invisible once pressed
+        window['-DISCONNECT-'].update(visible=True) #makes disconnect button visible
+    if event == '-DISCONNECT-': #when disconnect button is pressed
         tunnel = False
-        window['-CONNECT-'].update(visible=True)
+        window['-CONNECT-'].update(visible=True) #the above but reversed
         window['-DISCONNECT-'].update(visible=False)
 
 window.close()
